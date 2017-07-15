ae433f42e3f6aa85cfbe14d0911e8bce53049a85
Blue <ericmager@gmx.de>
2017-07-15 14:03:58 +0200

    indent server

--- basic/server.lua
+++ basic/server.lua
@@ -5,29 +5,29 @@
 AddEventHandler('chatMessage', function(source, name, msg)
 	sm = stringsplit(msg, " ");
 	if sm[1] == "/ooc" then
-	CancelEvent()
-	TriggerClientEvent('chatMessage', -1, "OOC | " .. name, { 196, 255, 0 }, string.sub(msg,5))
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "OOC | " .. name, { 196, 255, 0 }, string.sub(msg,5))
 	elseif sm[1] == "/pts" then
-    CancelEvent()
-   	TriggerClientEvent("yt:PlaceCone", source)
-    TriggerClientEvent('chatMessage', source, 'Police ', { 13, 89, 146 }, 'Cone Placed!')
+		CancelEvent()
+		TriggerClientEvent("yt:PlaceCone", source)
+		TriggerClientEvent('chatMessage', source, 'Police ', { 13, 89, 146 }, 'Cone Placed!')
 	elseif sm[1] == "/911" then
-	CancelEvent()
-	TriggerClientEvent('chatMessage', -1, "911 | " .. name, { 13, 89, 146 }, string.sub(msg,5))
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "911 | " .. name, { 13, 89, 146 }, string.sub(msg,5))
 	elseif sm[1] == "/tow" then
-	CancelEvent()
-	TriggerClientEvent('chatMessage', -1, "Tow | " .. name, { 160, 165, 13 }, string.sub(msg,5))
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "Tow | " .. name, { 160, 165, 13 }, string.sub(msg,5))
 	elseif sm[1] == "/twt" then
-	CancelEvent()
-	TriggerClientEvent('chatMessage', -1, "Twitter | " .. name, { 0, 255, 255 }, string.sub(msg,5))
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "Twitter | " .. name, { 0, 255, 255 }, string.sub(msg,5))
 	elseif sm[1] == "/onduty" then
-	TriggerClientEvent("yt:policeLoadout", source)
-    CancelEvent()
-    TriggerClientEvent('chatMessage', source, 'Police Loadout', { 13, 89, 146 }, 'Loadout Given!')
+		TriggerClientEvent("yt:policeLoadout", source)
+		CancelEvent()
+		TriggerClientEvent('chatMessage', source, 'Police Loadout', { 13, 89, 146 }, 'Loadout Given!')
 	elseif sm[1] == "/dis" then
-    CancelEvent()
-    TriggerClientEvent('chatMessage', -1, "Police Dispatch | " .. name, { 13, 89, 146 }, string.sub(msg,5))
-    end
+		CancelEvent()
+		TriggerClientEvent('chatMessage', -1, "Police Dispatch | " .. name, { 13, 89, 146 }, string.sub(msg,5))
+	end
 end)
 
 
