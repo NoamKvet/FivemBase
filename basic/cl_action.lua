ebcea68be7ba540f1d56b10b6190b8ef5fe0b752
NoamKvet <noamchickv@gmail.com>
2017-12-09 12:23:36 +0200

    Add files via upload

--- basic/cl_action.lua
+++ basic/cl_action.lua
@@ -1,129 +1,80 @@
 -- [[-----------------------------------------------------------------------
 
-	-- ActionMenu By WolfKnight
+	-- ActionMenu By Darklandz
 	-- Script By Kvetz
 	-- Thanks Titch For The Help!
 
 -- ---------------------------------------------------------------------]]--
 
+local playermenuEnabled = false 
+
+
 RegisterNetEvent("ToggleActionmenu")
 AddEventHandler("ToggleActionmenu", function()
-  ToggleActionMenu()
+  openGui()
 end)
--- Define the variable used to open/close the menu 
-local menuEnabled = false 
-
---[[------------------------------------------------------------------------
-	ActionMenu Toggle
-	Calling this function will open or close the ActionMenu. 
-------------------------------------------------------------------------]]--
-function ToggleActionMenu()
-	-- Make the menuEnabled variable not itself 
-	-- e.g. not true = false, not false = true 
-	menuEnabled = not menuEnabled
-
-	if ( menuEnabled ) then 
-		-- Focuses on the NUI, the second parameter toggles the 
-		-- onscreen mouse cursor. 
-		SetNuiFocus( true, true )
-
-		-- Sends a message to the JavaScript side, telling it to 
-		-- open the menu. 
-		SendNUIMessage({
-			showmenu = true 
-		})
-	else 
-		-- Bring the focus back to the game
-		SetNuiFocus( false )
-
-		-- Sends a message to the JavaScript side, telling it to
-		-- close the menu.
-		SendNUIMessage({
-			hidemenu = true 
-		})
-	end 
-end 
-
---[[------------------------------------------------------------------------
-	ActionMenu HTML Callbacks
-	This will be called every single time the JavaScript side uses the
-	sendData function. The name of the data-action is passed as the parameter
-	variable data. 
-------------------------------------------------------------------------]]--
-RegisterNUICallback( "ButtonClick", function( data, cb ) 
-	if ( data == "button1" ) then 
-		chatPrint( "You are a cop! go arrest some pepole!" )
-		TriggerEvent("yt:cop", source)
-	elseif ( data == "button2" ) then 
-		chatPrint( "You are a medic!! go save some pepole!" )
-	    TriggerEvent("yt:medic", source)
-	elseif ( data == "button3" ) then 
-		chatPrint( "You are a firefighter! go save some pepole!" )
-		TriggerEvent("yt:firefighter", source)
-	elseif ( data == "button4" ) then 
-		chatPrint( "You are a civilian! go rob some places!" )
-		TriggerEvent("yt:civilian", source)
-	elseif ( data == "exit" ) then 
-		-- We toggle the ActionMenu and return here, otherwise the function 
-		-- call below would be executed too, which would just open the menu again 
-		ToggleActionMenu()
-		return 
-	end 
-
-	-- This will only be called if any button other than the exit button is pressed
-	ToggleActionMenu()
-end )
 
 
---[[------------------------------------------------------------------------
-	ActionMenu Control and Input Blocking 
-	This is the main while loop that opens the ActionMenu on keypress. It 
-	uses the input blocking found in the ES Banking resource, credits to 
-	the authors.
-------------------------------------------------------------------------]]--
-Citizen.CreateThread( function()
-	-- This is just in case the resources restarted whilst the NUI is focused. 
-	SetNuiFocus( false )
 
-	while true do 
-		-- Control ID 20 is the 'Z' key by default 
-		-- Use https://wiki.fivem.net/wiki/Controls to find a different key 
+function openGui()
+	SetNuiFocus(true, true)
+  	SendNUIMessage({showPlayerMenu = true})
+end
 
+-- Close Gui and disable NUI
+function closeGui()
+	SendNUIMessage({hidePlayerMenu = true})
+	SetNuiFocus(false)
+  	playermenuEnabled = false 
+end
 
-	    if ( menuEnabled ) then
-            local ped = GetPlayerPed( -1 )	
-
-            DisableControlAction( 0, 1, true ) -- LookLeftRight
-            DisableControlAction( 0, 2, true ) -- LookUpDown
-            DisableControlAction( 0, 24, true ) -- Attack
-            DisablePlayerFiring( ped, true ) -- Disable weapon firing
-            DisableControlAction( 0, 142, true ) -- MeleeAttackAlternate
-            DisableControlAction( 0, 106, true ) -- VehicleMouseControlOverride
-        end
-
-		Citizen.Wait( 0 )
-	end 
-end )
 
 function chatPrint( msg )
 	TriggerEvent( 'chatMessage', "Loadout", { 255, 255, 255 }, msg )
 end 
 
 
+-- NUI Callback Methods
 
+RegisterNUICallback('close', function(data, cb)  
+  closeGui()
+  cb('ok')
+end)
 
+-- NUI Civilian Callback
 
+RegisterNUICallback('civLoad', function(data, cb) 
+  	chatPrint( "You are a civilian! go rob some places!" )
+	TriggerEvent("yt:civilian", source)
+  	closeGui()
+  	cb('ok')
+end)
 
+-- NUI Medic Callback
 
+RegisterNUICallback('medLoad', function(data, cb)
+  chatPrint( "You are a medic!! go save some pepole!" )
+  TriggerEvent("yt:medic", source)
+  closeGui()
+  cb('ok')
+end)
 
+-- NUI Police Callback
 
+RegisterNUICallback('polLoad', function(data, cb)
+  	chatPrint( "You are a cop! go arrest some pepole!" )
+	TriggerEvent("yt:cop", source)
+  	closeGui()
+  	cb('ok')
+end)
 
+-- NUI Firefighter Callback
 
-
-
-
-
-
-
+RegisterNUICallback('fireLoad', function(data, cb)
+  	chatPrint( "You are a firefighter! go save some pepole!" )
+	TriggerEvent("yt:firefighter", source)
+  	closeGui()
+  	cb('ok')
+end)
 
 
