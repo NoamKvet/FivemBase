161eaef0469b233f9ca8e707de84a11d49a06df7
Starystars67 <starystars67@gmail.com>
2017-12-22 17:03:38 +0000

    /loadout Fix

--- basic/cl_action.lua
+++ basic/cl_action.lua
@@ -8,18 +8,34 @@
 
 local menuEnabled = false 
 
+RegisterNetEvent("ToggleActionmenu")
+AddEventHandler("ToggleActionmenu", function()
+	ToggleActionMenu()
+end)
 
 function ToggleActionMenu()
+	-- Make the menuEnabled variable not itself 
+	-- e.g. not true = false, not false = true 
 	menuEnabled = not menuEnabled
+
 	if ( menuEnabled ) then 
-		SetNuiFocus( true, true ) 
+		-- Focuses on the NUI, the second parameter toggles the 
+		-- onscreen mouse cursor. 
+		SetNuiFocus( true, true )
+
+		-- Sends a message to the JavaScript side, telling it to 
+		-- open the menu. 
 		SendNUIMessage({
-			showmenu = true 
+			showPlayerMenu = true 
 		})
 	else 
+		-- Bring the focus back to the game
 		SetNuiFocus( false )
+
+		-- Sends a message to the JavaScript side, telling it to
+		-- close the menu.
 		SendNUIMessage({
-			hidemenu = true 
+			hidesPlayerMenu = true 
 		})
 	end 
 end 
